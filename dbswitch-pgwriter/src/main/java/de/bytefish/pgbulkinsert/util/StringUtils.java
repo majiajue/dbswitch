dfa317ebe8289271064cfad89dff2bb5f9bac7a4
tangyibo <tangyibo@ruijie.com.cn>
2020-03-11 11:10:54 +0800

    fix bug: 处理UTF8编码文本中的0x00

--- dbswitch-pgwriter/src/main/java/de/bytefish/pgbulkinsert/util/StringUtils.java
+++ dbswitch-pgwriter/src/main/java/de/bytefish/pgbulkinsert/util/StringUtils.java
@@ -16,4 +16,28 @@ public class StringUtils {
         return value.getBytes(utf8Charset);
     }
 
+	/**
+	 * 字符串中发现非法字符 0x00，将其删除
+	 * 
+	 * @author tang
+	 * 
+	 * @param data 处理前的字符串
+	 * @return 处理后的字符串
+	 */
+	public static String escapeString(String data) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < data.length(); ++i) {
+			char c = data.charAt(i);
+			switch (c) {
+			case 0x00:
+				continue;
+			default:
+				break;
+			}
+
+			sb.append(c);
+		}
+		return sb.toString();
+	}
 }
